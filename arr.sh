39187e76834f83ee0116357d9681a4b12731ca11
yohanesthree <m26414007@john.petra.ac.id>
2016-11-15 13:12:35 +0700

    +arr.sh

---
+++ arr.sh
@@ -0,0 +1,13 @@
+mth[0]=31
+mth[1]=28
+mth[2]=31
+mth[3]=30
+mth[4]=31
+mth[5]=30
+mth[6]=31
+mth[7]=31
+mth[8]=30
+mth[9]=31
+mth[10]=30
+mth[11]=31
+echo "Banyak hari dalam bulan Mei adalah ${mth[4]} hari"
